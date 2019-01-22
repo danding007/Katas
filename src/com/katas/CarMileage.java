55de6201492c27f327465dfbd775b95819921867
Danding007 <406473668@qq.com>
2019-01-23 02:06:31 +0800

    code refactoring should return interesting when digit followed by all zeros

--- src/com/katas/CarMileage.java
+++ src/com/katas/CarMileage.java
@@ -6,13 +6,20 @@ public class CarMileage {
 	public static final int INTERESTING = 2;
 
 	public static int isInteresting(int number, int[] awesomePhrases) {
-		if (number == 100) {
-			return INTERESTING;
-		}
-		if (number == 200) {
+		String mileage = String.valueOf(number);
+		if (number >= 100 && isAnyDigitFollowedByAllZeros(mileage)) {
 			return INTERESTING;
 		}
 		return NOT_INTERESTING;
+	}
+
+	private static boolean isAnyDigitFollowedByAllZeros(String mileage) {
+		for (int i = 1; i < mileage.length(); i++) {
+			if (mileage.charAt(i) != '0') {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
