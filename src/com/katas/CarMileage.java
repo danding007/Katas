a4fc0497b801df2c20c1b3f6e9a3b95db32137bf
Danding007 <406473668@qq.com>
2019-01-23 00:57:41 +0800

    Refactoring rename magic number

--- src/com/katas/CarMileage.java
+++ src/com/katas/CarMileage.java
@@ -3,10 +3,11 @@ package com.katas;
 public class CarMileage {
 
 	public static final int NOT_INTERESTING = 0;
+	public static final int INTERESTING = 2;
 
 	public static int isInteresting(int number, int[] awesomePhrases) {
 		if (number == 100) {
-			return 2;
+			return INTERESTING;
 		}
 		return NOT_INTERESTING;
 	}
