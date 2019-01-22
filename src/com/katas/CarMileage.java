090cc30c8faf06c26346996402fb73856e198478
Danding007 <406473668@qq.com>
2019-01-23 00:57:13 +0800

    should return interesting given mileage is 100

--- src/com/katas/CarMileage.java
+++ src/com/katas/CarMileage.java
@@ -5,6 +5,9 @@ public class CarMileage {
 	public static final int NOT_INTERESTING = 0;
 
 	public static int isInteresting(int number, int[] awesomePhrases) {
+		if (number == 100) {
+			return 2;
+		}
 		return NOT_INTERESTING;
 	}
 
--- test/com/katas/CarMileageTest.java
+++ test/com/katas/CarMileageTest.java
@@ -10,4 +10,9 @@ public class CarMileageTest {
 	public void should_return_not_interesting_given_mileage_less_than_100() {
 		assertEquals(0, CarMileage.isInteresting(99, new int[]{256}));
 	}
+
+	@Test
+	public void should_return_interesting_given_mileage_is_100() {
+		assertEquals(2, CarMileage.isInteresting(100, new int[]{256}));
+	}
 }
